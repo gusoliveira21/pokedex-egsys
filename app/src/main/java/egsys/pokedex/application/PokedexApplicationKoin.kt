6d6c520171f9eb3def2f744f8d9172cac73ae146
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-06-26 15:49:09 -0300

    feat: setup koin and add new dependencies

---
+++ app/src/main/java/egsys/pokedex/application/PokedexApplicationKoin.kt
@@ -0,0 +1,22 @@
+package egsys.pokedex.application
+
+import android.app.Application
+import egsys.pokedex.di.mainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class PokedexApplicationKoin : Application()  {
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@PokedexApplicationKoin)
+            modules(mainModule)
+        }
+    }
+}
---
+++ app/src/main/java/egsys/pokedex/di/mainModule.kt
@@ -0,0 +1,7 @@
+package egsys.pokedex.di
+
+import org.koin.dsl.module
+
+val mainModule = module {
+
+}
